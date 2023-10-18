450a8f4f5fda10054fcbd9cb106b7b0760f8c2d5
azat <eric@example.com>
2023-10-18 08:29:03 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+    const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    if (number === 0) {
+        return "zero";
+    }
+
+    if (number < 10) {
+        return ones[number];
+    }
+
+    if (number >= 11 && number < 20) {
+        return teens[number - 10];
+    }
+
+    if (number < 100) {
+        const tenDigit = Math.floor(number / 10);
+        const oneDigit = number % 10;
+        return `${tens[tenDigit]} ${oneDigit !== 0 ? ones[oneDigit]:""}`.trim();
+    }
+
+    if(number < 1000) {
+        const hundredDigit = Math.floor(number / 100);
+        const remainder = number % 100;
+        return `${ones[hundredDigit]} hundred ${remainder !==0 ? toReadable(remainder): ""}`.trim();
+    }
 }
+
+
